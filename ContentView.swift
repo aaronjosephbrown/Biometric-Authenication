58ba55caf28ec9b70e699c3d2e959d9756ca08f1
Aaron J. Brown <39014977+aaronjosephbrown@users.noreply.github.com>
2023-10-10 13:05:21 -0500

    Create ContentView.swift

---
+++ ContentView.swift
@@ -0,0 +1,41 @@
+import LocalAuthentication
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var isUnlocked = false
+    
+    var body: some View {
+        VStack {
+            if isUnlocked {
+                Text("Unlocked")
+            } else {
+                Text("locked")
+            }
+        }
+        .onAppear(perform: authenticate)
+    }
+    
+    func authenticate () {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthentication, error: &error) {
+            let reason = "We need to unlock you data"
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                if success {
+                    isUnlocked = true
+                } else {
+                    // there was a problem
+                }
+            }
+        } else {
+            // no biometrics
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
